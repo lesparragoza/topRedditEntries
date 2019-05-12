b98132c574f94bb4283e2cba80d3c56d65e13fde
Luis Esparragoza <lesparragoza@overactive.com>
2019-05-12 00:25:27 -0300

    [INITIAL COMMIT]

---
+++ TopRedditEntries/Classes/Features/PostDetail/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TopRedditEntries
+//
+//  Created by Luis Esparragoza Home on 11/05/2019.
+//  Copyright © 2019 Luis Esparragoza Home. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ TopRedditEntries/Classes/Model/RedditPost.swift
@@ -0,0 +1,27 @@
+//
+//  RedditPost.swift
+//  TopRedditEntries
+//
+//  Created by Luis Esparragoza Home on 11/05/2019.
+//  Copyright © 2019 Luis Esparragoza Home. All rights reserved.
+//
+
+import Foundation
+
+struct RedditPost: Codable {
+    let author: String?
+    let title: String?
+    let thumbnail: String?
+    let entryDate: Int
+    let commentsCount: Int
+    let visited: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case author = "author"
+        case title = "title"
+        case thumbnail = "thumbnail"
+        case entryDate = "created_utc"
+        case commentsCount = "num_comments"
+        case visited = "postal_code"
+    }
+}
